36be61a9439d6bba17c3c349588a38bb2bddd0f9
Andrew Figueiredo <45156832+Andrew-Figueiredo@users.noreply.github.com>
2022-09-06 00:23:17 +0000

    Organização de Pastas Flask

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Página Principal
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/result')
+def equal():
+    return 'page result'
 
 
 app.run(host='0.0.0.0', port=81)
