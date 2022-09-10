b6c8a02f443ab4b49818f7c31ff9041daead51d7
Andrew Figueiredo <andrewdw18@gmail.com>
2022-09-09 21:27:49 -0300

    Finalizado a Parte de python e Operações com HTML, Falta somente o CSS

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+def calculator(num1:int, num2:int, op:str):
+    if op == '+':
+        return str(num1 + num2)
+    elif op == '-':
+        return str(num1 - num2)
+    elif op == '*':
+        return str(num1 * num2)
+    elif op == '/':
+        if num2 == 0:
+            return 'Divisão por zero!'
+        else:
+            return str(num1/num2)
+    else:
+        return 'Operação Inviável de ser realizada!'
 # Página Principal
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/result')
+@app.post('/result')
 def equal():
-    return 'Calculo Realizado'
+    number1 = int(request.form['FN'])
+    number2 = int(request.form['SN'])
+    operador = request.form['OP']
+
+    resultado = calculator(number1, number2, operador)
+    print(resultado)
+    return render_template('result.html', resultado = resultado)
+
 
 
 app.run(host='0.0.0.0', port=81, debug=True)
